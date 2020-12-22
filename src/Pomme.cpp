55d5134e8447a5ad24daa4c1e262d71df4f53fb0
NiiRoZz <extazziii1000@gmail.com>
2020-12-22 20:16:59 +0100

    Better working like that

--- src/Pomme.cpp
+++ src/Pomme.cpp
@@ -3,10 +3,35 @@
 
 #include "Pomme.h"
 
-using namespace std;
+#include <antlr4-runtime.h>
+#include "PommeLexer.h"
+#include "PommeParser.h"
+
+using namespace Pomme;
 
 int main()
 {
-	cout << "Hello CMake." << endl;
+	//std::ifstream stream;
+	//stream.open("input.scene");
+
+	antlr4::ANTLRInputStream input("testtest");
+	PommeLexer lexer(&input);
+	antlr4::CommonTokenStream tokens(&lexer);
+
+	tokens.fill();
+	for (auto token : tokens.getTokens()) {
+		std::cout << token->toString() << std::endl;
+	}
+
+	PommeParser parser(&tokens);
+
+	parser.name()->accept();
+	
+	/*antlr4::tree::ParseTree *tree = parser.stored_definition();
+
+	parser.main();
+
+	Pomme::PommeParser::FileContext* tree = parser.file();*/
+
 	return 0;
 }
